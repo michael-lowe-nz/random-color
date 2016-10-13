2f9014ff50da4c2f31f0d7bc95ed0b46cd45d861
Michael Lowe <mikkeyfluttie@gmail.com>
2016-10-13 16:43:38 +1300

    Add saved colors function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
-document.getElementById("button").addEventListener("click",function(){
-  var color = generateColor();
-  $("#screenWrapper").css("background-color",color);
-  $("#hex-value").html(color);
+var currentColor = "#000000"
+var savedColors = [];
+
+document.getElementById("generate").addEventListener("click",function(){
+  currentColor = generateColor();
+  $("#screenWrapper").css("background-color",currentColor);
+  $("#hex-value").html(currentColor);
 });
+
+document.getElementById("save").addEventListener("click",saveColor);
+
+
 
 function generateColor(){
   return '#' + Math.random().toString(16).slice(2, 8);
+}
+
+function saveColor() {
+  savedColors.push(currentColor);
+  console.log("saving color...");
+  $("#saved-box").html("");
+  for (var i = 0; i < savedColors.length; i++){
+    var myColor = savedColors[i];
+    $("#saved-box").append('<div class="saved-color" style="background-color:'+myColor+';"></div>');
+    // $("#saved-box:nth-child(i+1)").css("background-color",currentColor);
+  }
 }
